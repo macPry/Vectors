86d07cc44391f1dced5ff7927e0e2932c22770f7
macPry <maciej.przybyl@elpassion.pl>
2017-06-12 21:08:33 +0200

    Animate view and change color

--- app/src/main/java/pl/elpassion/vector/MainActivity.kt
+++ app/src/main/java/pl/elpassion/vector/MainActivity.kt
@@ -1,8 +1,10 @@
 package pl.elpassion.vector
 
+import android.animation.AnimatorInflater
 import android.graphics.drawable.Animatable
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         (image.drawable as? Animatable)?.start()
         (line.drawable as? Animatable)?.start()
+
+        lineView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent))
+        val objectAnimator = AnimatorInflater.loadAnimator(this, R.animator.line_view_animator)
+        objectAnimator.setTarget(lineView)
+        objectAnimator.start()
     }
+
 }
+
