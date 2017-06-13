7dd7e4c89661e16c5d750febce2d409f1dec11a5
macPry <maciej.przybyl@elpassion.pl>
2017-06-13 13:33:14 +0200

    View transition

--- app/src/main/java/pl/elpassion/vector/MainActivity.kt
+++ app/src/main/java/pl/elpassion/vector/MainActivity.kt
@@ -1,10 +1,13 @@
 package pl.elpassion.vector
 
-import android.animation.AnimatorInflater
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator.INFINITE
 import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +15,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        (line.drawable as? Animatable)?.start()
-
-        lineView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent))
-        val objectAnimator = AnimatorInflater.loadAnimator(this, R.animator.line_view_animator)
-        objectAnimator.setTarget(lineView)
-        objectAnimator.start()
+        init()
     }
 
-}
+    fun init() {
+        (line.drawable as? Animatable)?.start()
 
+        lineView.viewTreeObserver.addOnGlobalLayoutListener {
+            lineView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent))
+            val parentWidth = (lineView.parent as View).width
+            ObjectAnimator.ofFloat(lineView, "translationX", -parentWidth.toFloat(), parentWidth.toFloat()).apply {
+                duration = 2000
+                repeatCount = INFINITE
+                interpolator = AccelerateDecelerateInterpolator()
+            }.start()
+        }
+    }
+}
