708b7fc0193b4066d39eb1cc34a813898257cda0
macPry <maciej.przybyl@elpassion.pl>
2017-06-13 15:20:13 +0200

    Fade out

--- app/src/main/java/pl/elpassion/vector/MainActivity.kt
+++ app/src/main/java/pl/elpassion/vector/MainActivity.kt
@@ -1,10 +1,13 @@
 package pl.elpassion.vector
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator.INFINITE
 import android.graphics.drawable.Animatable
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -13,30 +16,61 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val lineVectorAnimation by lazy { line.drawable as? Animatable }
+    val lineViewAnimation by lazy { AnimatorSet() }
+    val fadeOutAnimation by lazy { AnimatorSet() }
+    var isFadeOut = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        init()
+        Handler().postDelayed({ init() }, 1000)
+        buttonFade.setOnClickListener { fade() }
     }
 
     fun init() {
-        (line.drawable as? Animatable)?.start()
+        lineVectorAnimation?.start()
 
-        lineView.viewTreeObserver.addOnGlobalLayoutListener {
-            lineView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent))
-            val parentWidth = (lineView.parent as View).width
-            val objectAnimatorTranslationX = ObjectAnimator.ofFloat(lineView, "translationX", -parentWidth.toFloat(), parentWidth.toFloat()).apply {
-                duration = 1000
-                repeatCount = INFINITE
-                interpolator = AccelerateDecelerateInterpolator()
-            }
-            val objectAnimatorScaleX = ObjectAnimator.ofFloat(lineView, "scaleX", 1f, 0f).apply {
-                duration = 1000
-                repeatCount = INFINITE
-            }
-            AnimatorSet().apply {
-                playTogether(objectAnimatorTranslationX, objectAnimatorScaleX)
-            }.start()
+        lineView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent))
+        val parentWidth = (lineView.parent as View).width
+        val objectAnimatorTranslationX = ObjectAnimator.ofFloat(lineView, "translationX", -parentWidth.toFloat(), parentWidth.toFloat()).apply {
+            repeatCount = INFINITE
+            interpolator = AccelerateDecelerateInterpolator()
         }
+        val objectAnimatorScaleX = ObjectAnimator.ofFloat(lineView, "scaleX", 1f, 0f).apply {
+            repeatCount = INFINITE
+        }
+        lineViewAnimation.apply {
+            playTogether(objectAnimatorTranslationX, objectAnimatorScaleX)
+            duration = 1000
+        }.start()
+
+        fadeOutAnimation.apply {
+            playTogether(ObjectAnimator.ofFloat(line, "alpha", 1f, 0f), ObjectAnimator.ofFloat(lineView, "alpha", 1f, 0f))
+            duration = 1000
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    lineVectorAnimation?.stop()
+                    line.visibility = View.GONE
+                    lineViewAnimation.end()
+                    lineView.visibility = View.GONE
+                }
+            })
+        }
+    }
+
+    fun fade() {
+        if (isFadeOut) {
+            line.visibility = View.VISIBLE
+            line.alpha = 1f
+            lineView.visibility = View.VISIBLE
+            lineView.alpha = 1f
+            lineViewAnimation.start()
+            lineVectorAnimation?.start()
+        } else {
+            fadeOutAnimation.start()
+        }
+        isFadeOut = !isFadeOut
     }
 }
